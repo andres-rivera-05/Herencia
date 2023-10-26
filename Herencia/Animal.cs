f41ba3072c517aebfb352618f2a15f3e0afb6299
andres-rivera-05 <cristianrivera161141@gmail.com>
2023-10-26 16:49:51 -0600

    Agregar archivos de proyecto.

---
+++ Herencia/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    internal class Animal
+    {
+        public string? nombre;
+        public string? color;
+        public string? tamanio;
+        public string? familia;
+
+        //Metodo para imprimir datos
+        public void imprimir()
+        {
+            if (validar())
+            {
+                Console.WriteLine("Nombre: " + nombre);
+                Console.WriteLine("Color: " + color);
+                Console.WriteLine("Tamañio: " + tamanio);
+                Console.WriteLine("Familia: " + familia);
+            }
+            else {
+                Console.WriteLine("Los campos no estan completos");
+            }
+        }
+
+        //Metodos para validar los datos
+        public bool validar() {
+            if (nombre != null && color != null && tamanio != null && familia != null)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }  
+    }
+}
---
+++ Herencia/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Herencia;
+
+//instacia de objeto
+Vertebrado vertebrado = new Vertebrado();
+Vertebrado vertebradoDos = new Vertebrado();
+
+vertebrado.nombre = "Gato";
+vertebrado.color = "negro";
+vertebrado.tamanio = "1 centimetro";
+vertebrado.familia = "Aguacatero";
+vertebrado.tipo = "mamifero";
+vertebrado.tienePatas = true;   
+vertebrado.esDomestico = true;
+vertebrado.esHevivoro = false;  
+vertebrado.esCarnivoro = true;
+
+vertebrado.imprimir();
+
+Console.WriteLine();
+
+vertebrado.nombre = "Perro";
+vertebradoDos.color = "cafe";
+vertebradoDos.tamanio = "mediano";
+vertebradoDos.familia = "Pastor Aleman";
+vertebradoDos.tipo = "mamifero";
+vertebradoDos.tienePatas = true;
+vertebradoDos.esDomestico = true;
+vertebradoDos.esHevivoro = false;
+vertebradoDos.esCarnivoro = true;
+
+vertebradoDos.imprimir();
+
+
+
