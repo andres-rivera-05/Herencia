98cf5e9b33a6b5b5f8956f29819bd511a41b0f60
andres-rivera-05 <cristianrivera161141@gmail.com>
2023-10-26 17:11:12 -0600

    creando clase invertebrado

---
+++ Herencia/Invertebrado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    internal class Invertebrado : Animal
+    {
+        public string? tipo;
+        public bool? tienePatas;
+        public bool? numeroPatas;
+        public bool? tieneConcha;
+
+
+        public void imprimir() { 
+
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Color: " + color);
+            Console.WriteLine("Tamañio: " + tamanio);
+            Console.WriteLine("Familia: " + familia);
+            Console.WriteLine("Tipo: " + tipo);
+            Console.WriteLine("Tiene Patas: " + tienePatas);
+            Console.WriteLine("Numero de patas: " + numeroPatas);
+            Console.WriteLine("Tiene Concha: " + tieneConcha);
+
+        }
+    }
+}
